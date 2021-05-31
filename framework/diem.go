2b084df4dfe179753bf31d27483ecaf83cc4170c
stevealexrs <59494379+stevealexrs@users.noreply.github.com>
2021-06-01 00:03:35 +0800

    Created diem helper functions

---
+++ framework/diem.go
@@ -0,0 +1,170 @@
+package framework
+
+import (
+	"time"
+
+	"github.com/diem/client-sdk-go/diemclient"
+	"github.com/diem/client-sdk-go/diemjsonrpctypes"
+	"github.com/diem/client-sdk-go/diemkeys"
+	"github.com/diem/client-sdk-go/diemsigner"
+	"github.com/diem/client-sdk-go/diemtypes"
+	"github.com/diem/client-sdk-go/stdlib"
+)
+
+type DiemHandler struct {
+	client diemclient.Client
+	parentVASP *diemkeys.Keys
+	chainId byte
+}
+
+type Gas struct {
+	Amount uint64
+	Price uint64
+	Code diemtypes.TypeTag
+}
+
+func MakeGas(amount, price uint64, code string) Gas {
+	return Gas{
+		Amount: amount,
+		Price: price,
+		Code: diemtypes.Currency(code),
+	}
+}
+
+func NewVASPAccount(publicKey, privateKey string) (*diemkeys.Keys, error) {
+	public, err := diemkeys.NewEd25519PublicKeyFromString(publicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	private, err := diemkeys.NewEd25519PrivateKeyFromString(privateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return diemkeys.NewKeysFromPublicAndPrivateKeys(public, private), nil
+}
+
+func NewDiemHandler(chainId byte, url string, parentVASP *diemkeys.Keys) *DiemHandler {
+	return &DiemHandler{
+		diemclient.New(chainId, url),
+		parentVASP,
+		chainId,
+	}
+}
+
+func (handler *DiemHandler) submitAndWait(sender *diemkeys.Keys, gas Gas, script diemtypes.Script) (*diemjsonrpctypes.Transaction, error) {
+	address := sender.AccountAddress()
+
+Retry:
+	account, err := handler.client.GetAccount(address)
+	if err != nil {
+		if _, ok := err.(*diemclient.StaleResponseError); ok {
+			// retry to hit another server if got stale response
+			goto Retry
+		}
+		return nil, err
+	}
+
+	sequence := account.SequenceNumber
+	expirationDuration := 30 * time.Second
+	expiration := uint64(time.Now().Add(expirationDuration).Unix())
+	
+	txn := diemsigner.Sign(
+		sender,
+		address,
+		sequence,
+		script,
+		1_000_000, 0, "XUS",
+		expiration,
+		handler.chainId,
+	)
+
+	err = handler.client.SubmitTransaction(txn)
+	if err != nil {
+		if _, ok := err.(*diemclient.StaleResponseError); !ok {
+			return nil, err
+		} 
+		// ignore *diemclient.StaleResponseError as we know
+		// submit probably succeed even hit a stale server
+	}
+
+	return handler.client.WaitForTransaction2(txn, expirationDuration)
+}
+
+// The transaction should be generated and signed client side.
+// The timeout is for waiting time
+func (handler *DiemHandler) SubmitSignedTransactionAndWait(signedTxnHex string, timeout time.Duration) (*diemjsonrpctypes.Transaction, error) {
+	err := handler.client.Submit(signedTxnHex)
+	if err != nil {
+		if _, ok := err.(*diemclient.StaleResponseError); !ok {
+			return nil, err
+		} 
+		// ignore *diemclient.StaleResponseError as we know
+		// submit probably succeed even hit a stale server
+	}
+
+	return handler.client.WaitForTransaction3(signedTxnHex, timeout)
+}
+
+// Needs to verify user actually hold the private key by signing
+func (handler *DiemHandler) CreateChildAndWait(currency string, address string, authKey string, gas Gas) error {
+	childAddress, err := diemtypes.MakeAccountAddress(address)
+	if err != nil {
+		return err
+	}
+
+	childAuthKey, err := diemkeys.NewAuthKeyFromString(authKey)
+	if err != nil {
+		return err
+	}
+
+	_, err = handler.submitAndWait(
+		handler.parentVASP,
+		gas,
+		stdlib.EncodeCreateChildVaspAccountScript(
+			diemtypes.Currency(currency),
+			childAddress,
+			childAuthKey.Prefix(),
+			true,
+			0,
+		),
+	)
+	return err
+}
+
+// The sender must be custodial
+// Amount is in microdiem
+func (handler *DiemHandler) PeerToPeerTransferAndWait(sender *diemkeys.Keys, recipientAddr string, currency string, amount uint64, metadata []byte, gas Gas) (*diemjsonrpctypes.Transaction, error) {
+	address, err := diemtypes.MakeAccountAddress(recipientAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	return handler.submitAndWait(
+		sender,
+		gas,
+		stdlib.EncodePeerToPeerWithMetadataScript(
+			diemtypes.Currency(currency),
+			address,
+			amount,
+			metadata,
+			nil,
+		),
+	)
+}
+
+// Get account information using its address
+func (handler *DiemHandler) AccountInfo(address string) (*diemjsonrpctypes.Account, error) {
+	accAddress, err := diemtypes.MakeAccountAddress(address)
+	if err != nil {
+		return nil, err
+	}
+
+	return handler.client.GetAccount(accAddress)
+}
+
+
+
+
+
---
+++ framework/diem_test.go
@@ -0,0 +1,66 @@
+package framework
+
+import (
+	"testing"
+
+	"github.com/diem/client-sdk-go/diemkeys"
+	"github.com/diem/client-sdk-go/testnet"
+)
+
+var diemHandler = NewDiemHandler(
+	testnet.ChainID,
+	testnet.URL,
+	testnet.GenAccount(),
+)
+
+var testGas = MakeGas(1000000, 0, "XUS")
+
+func TestCreateChildAndWait(t *testing.T) {
+	
+	t.Logf("Address: %s", diemHandler.parentVASP.AccountAddress().Hex())
+	key := diemkeys.MustGenKeys()
+
+		err := diemHandler.CreateChildAndWait(
+			"XUS",
+			key.AccountAddress().Hex(),
+			key.AuthKey().Hex(),
+			testGas,
+		)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+}
+
+func TestPeerToPeerTransferAndWait(t *testing.T) {
+	account := testnet.GenAccount()
+	t.Logf("\nParent Address: %s", diemHandler.parentVASP.AccountAddress().Hex())
+	t.Logf("\nRecipient Address: %s", account.AccountAddress().Hex())
+
+	testnet.MustMint(diemHandler.parentVASP.AuthKey().Hex(), 1000000, "XUS")
+
+	_, err := diemHandler.PeerToPeerTransferAndWait(
+		diemHandler.parentVASP,
+		account.AccountAddress().Hex(),
+		"XUS",
+		1000,
+		nil,
+		testGas,
+	)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
+
+func TestAccountInfo(t *testing.T) {
+	account := testnet.GenAccount()
+	t.Logf("\nAddress: %s", account.AccountAddress().Hex())
+
+	testnet.MustMint(account.AuthKey().Hex(), 103004000, "XUS")
+
+	info, err := diemHandler.AccountInfo(account.AccountAddress().Hex())
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	t.Logf("\nBalance: %s", info.Balances)
+}
